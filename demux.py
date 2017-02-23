84a0c1a85bdf77c5616b34b43e0e6ebd35329b12
Blair Edwards <blaired1@yahoo.co.uk>
2017-02-23 11:16:36 +0000

    Made demux.py mixed-output safe Ensures only one pin can be HIGH at a time

--- demux.py
+++ demux.py
@@ -11,90 +11,111 @@ import RPi.GPIO as GPIO
 import time
 import signal
 import sys
+import serial
 
 #  GPIO Setup
+relayCount = 2
 GPIO.setmode (GPIO.BOARD)  #  Pins use Broadcom's numbering
-gpioBtn = 11
 gpioPin0 = 13
 gpioPin1 = 15
 gpioPin2 = 16
 gpioPin3 = 18
-GPIO.setup (gpioBtn, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup (gpioPin0, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup (gpioPin1, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup (gpioPin2, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup (gpioPin3, GPIO.OUT, initial = GPIO.LOW)
+prevOutVal = -1
 
-GPIO.output (gpioPin2, GPIO.HIGH)
-input ("...")
-sys.exit (0)
 
 def selWithKbd ():
 	#  Asks for user to select a specific output of the demux
 	while True:
-		selOutRaw = input ("Which pin would you like to output on?  [0-3]  ")
+		selOutRaw = input ("Which pin would you like to output on?  [0-" + str (relayCount - 1) + "]  ")
 		try:
 			selOut = int (selOutRaw)
 		except:
 			print ("Please enter a valid integer.")
 		#  Check for suitable number
-		if selOut >= 0 and selOut < 4:
+		if selOut >= 0 and selOut < relayCount:
 			setOutput (selOut)
 		else:
-			print ("Please enter a number between 1 and 4.")
+			print ("Please enter a number between 0 and " + str (relayCount - 1) + ".")
 	return
 
 
-def selWithBtn ():
-	#  Cycles demux output on button press (polled)
-	print ("Please press the button to cycle the output.")
-	currOut = 0
+def cycleWithSerial ():
+	#  Cycles demux output when serial signal received
+	#  Set initial GPIO state
+	setOutput (0)
+	currOut = 1
+	
 	while True:
-		if (GPIO.input (gpioBtn)):
-			setOutput (currOut)
-			currOut += 1
-			if currOut > 3:
-				currOut = 0
-			time.sleep (0.01)
-			while (GPIO.input (gpioBtn)):
- 				pass
+		#  Loop until serial input received
+		while (port.read (10) .decode ("ASCII")) == "":
+			pass
+		#  Cycle GPIO output
+		setOutput (currOut)
+		#  Pre-set next GPIO state
+		if currOut >= (relayCount - 1):
+			currOut = 0
 		else:
-			time.sleep (0.01)
+			currOut = currOut + 1
+		
+		time.sleep (1)
 	return
 
 
 def setOutput (outVal):
 	#  Sets demux ouytput to a specific value
-	#  Check Pin 0
-	if outVal == 0:
-		print ("Writing to Pin 0")
-		GPIO.output (gpioPin0, GPIO.HIGH)
+	#  Declare the global variable 'prevOutVal'
+	global prevOutVal
+
+	#  Check if realy value is out of range (calling functions should handle this)
+	if outVal < 0:
+		print ("\nsetOutput:  outVal < 0")
+	elif outVal >= relayCount:
+		print ("\nsetOutput:  outVal >= relayCount")
+	
+	#  Relay value in range, let's proceed
 	else:
-		GPIO.output (gpioPin0, GPIO.LOW)
-	#  Check Pin 1
-	if outVal == 1:
-		print ("Writing to Pin 1")
-		GPIO.output (gpioPin1, GPIO.HIGH)
-	else:
-		GPIO.output (gpioPin1, GPIO.LOW)
-	#  Check Pin 2
-	if outVal == 2:
-		print ("Writing to Pin 2")
-		GPIO.output (gpioPin2, GPIO.HIGH)
-	else:
-		GPIO.output (gpioPin2, GPIO.LOW)
-	#  Check Pin 3
-	if outVal == 3:
-		print ("Writing to Pin 3")
-		GPIO.output (gpioPin3, GPIO.HIGH)
-	else:
-		GPIO.output (gpioPin3, GPIO.LOW)
+		if outVal == prevOutVal:
+			#  Nothing to do here
+			print ("Previous output of " + str (prevOutVal) + " the same, not changing anything")
+		else:
+			#  Set previously stored pin LOW
+			print ("Setting previous pin " + str (prevOutVal) + " LOW")
+			if prevOutVal == 0:
+				GPIO.output (gpioPin0, GPIO.LOW)
+			if prevOutVal == 1:
+				GPIO.output (gpioPin1, GPIO.LOW)
+			if prevOutVal == 2:
+				GPIO.output (gpioPin2, GPIO.LOW)
+			if prevOutVal == 3:
+				GPIO.output (gpioPin3, GPIO.LOW)
+			
+			#  Set the new pin HIGH
+			print ("Setting new pin " + str (outVal) + " HIGH")
+			if outVal == 0:
+				GPIO.output (gpioPin0, GPIO.HIGH)
+			if outVal == 1:
+				GPIO.output (gpioPin1, GPIO.HIGH)
+			if outVal == 2:
+				GPIO.output (gpioPin2, GPIO.HIGH)
+			if outVal == 3:
+				GPIO.output (gpioPin3, GPIO.HIGH)
+			
+			prevOutVal = outVal
+			
 	return
 
 
-def cleanup (signal, frame):
+def captureCC (signal, frame):
 	#  Cleanup function to exit gracefully
 	print ("\n\nctrl-c captured!  Cleaning up...")
+	cleanup ()
+
+
+def cleanup ():
 	#  Clean up the bound GPIO channels
 	GPIO.output (gpioPin0, GPIO.LOW)
 	GPIO.output (gpioPin1, GPIO.LOW)
@@ -105,15 +126,29 @@ def cleanup (signal, frame):
 	sys.exit (0)
 
 
+#  Only 4 relay positions programmed for now
+if relayCount < 1:
+	print ("\n\nYou can't have less than 1 output!\nPlease set 'relayCount' to a number greater than 0.")
+	cleanup ()
+elif relayCount > 4:
+	print ("\n\nToo many relays!\nIf you want to use more than 4, you will need to hard-code the GPIO addresses, add them to 'setOutput' and then edit here to squelch this code.")
+	cleanup ()
+
+	
 #  Setup ctrl-c capturing
-signal.signal (signal.SIGINT, cleanup)
+signal.signal (signal.SIGINT, captureCC)
+
+#  Setup Serial with PC
+port = serial.Serial ("/dev/ttyAMA0", baudrate=9600, timeout=0.1)
+
 
 while True:
-	modeSel = input ('Run in Keyboard ("k") or Button ("b") mode?  ')
+	modeSel = input ('Run in Keyboard ("k") or Serial ("s") mode?  ')
 	if modeSel == "k":
 		selWithKbd ()
-	elif modeSel == "b":
-		selWithBtn ()
+#		currTests ()
+	elif modeSel == "s":
+		cycleWithSerial ()
 	else:
 		print ("That was not a valid selection.")
 
